90f1b6effa06a3e1954c3eefa9f09a49a3ba3a36
Sakumatcho <sakumadaisuke32@gmail.com>
2025-01-17 14:49:17 +0000

    add #1 environment

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8080
+c.NotebookApp.token = ''
+c.NotebookApp.allow_root = True
+c.NotebookApp.open_browser = False
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.password_required = False
